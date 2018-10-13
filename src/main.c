230d7aef2d23636fac54b3f3b137c4684c41abdf
Sidney Carvalho <sydney_rdc@hotmail.com>
2018-10-13 00:30:58 -0300

    main code turned independent of extra files, message processing turned simpler

--- src/main.c
+++ src/main.c
@@ -1,102 +1,137 @@
-/* Test to Kilombo functions <Implementation>
+/* Main code to programming on Kilombo Simulator
  *
  * Author: Sidney Carvalho - sydney.rdc@gmail.com
- * Last Change: 2018 Sep 07 22:08:19
+ * Last Change: 2018 Oct 13 00:29:33
  */
 
-#include "communication.h"
+#include <kilombo.h>
+#include <math.h>
 
 // debug.h isn't a header file at all, it has function implementations, so be
 // careful to not include it at header files to avoid multiple definition errors.
 #ifndef SIMULATOR
-#define DEBUG          // for printf to serial port
+#define DEBUG                          // for printf to serial port
 #include <debug.h>
 #endif
 
+#define MAXN 20                        // maximum number of neighbours
+#define MAXMSG 10                      // maximum number of stored messages
+#define PI 3.14159265358979323846      // definition for π
+
+// neighbor datatype
+typedef struct {
+    uint16_t id;
+    uint8_t dist;
+    uint8_t n_neighbors;
+    uint32_t timestamp;
+} neighbor_t;
+
+// received message type
+typedef struct {
+    message_t msg;
+    uint8_t dist;
+} received_message_t;
+
+// declare global variables
+typedef struct {
+    neighbor_t neighbors[MAXN];
+    uint8_t n_neighbors;
+    uint8_t new_message;
+    char message_lock;
+    message_t transmit_msg;
+    received_message_t received_msg[MAXMSG];
+    uint16_t n_messages;
+} USERDATA;
+
 // register individual variables: the simulator ensures that this pointer points to
 // the data of the correct robot before calling any of the user program's functions.
-REGISTER_USERDATA(robot_data);
+REGISTER_USERDATA(USERDATA);
 
-// put your setup code here, to be run only once
-void setup() {
-    //seed the random number generator
-    rand_seed(kilo_uid + 1);
+// fill the message with the bot information
+void setup_message() {
+    // don't transmit while we are forming the message
+    mydata->message_lock = 1;
+    mydata->transmit_msg.type = NORMAL;
+    mydata->transmit_msg.data[0] = kilo_uid & 0xff;            // 0 low  id
+    mydata->transmit_msg.data[1] = kilo_uid >> 8;              // 1 high id
+    mydata->transmit_msg.data[2] = mydata->n_neighbors;        // 2 number of neighbors
 
-    // set the current time at the robot
-    mydata->timestamp = 0;
-
-    // set initial iteration as zero
-    mydata->iter = 0;
-
-    // set robot type
-    set_move_type(STOP);
-
-    // set robot state
-    set_state(LISTEN);
-
-    // set message lock as 0
+    mydata->transmit_msg.crc = message_crc(&mydata->transmit_msg);
     mydata->message_lock = 0;
-
-    // set the number of neighbours as zero
-    mydata->n_neighbors = 0;
-
-    // set the current light level
-    set_light_level(0);
-
-    // fill a message and send it
-    setup_message();
-
-    mydata->cycle_dt = 10;
 }
 
-// put your main code here, will be run repeatedly
-void loop() {
-    //receive messages
-    receive_inputs();
+// process the received message
+void process_message() {
+    // auxiliary index
+    int8_t i;
 
-    // select the leader
-    if(kilo_uid == 0) {
-        set_color(RGB(3, 0, 0));
-    } else {
-        /*uint32_t dt = kilo_uid == 1 ? 1 : 5;*/
-        uint8_t min_dist = 45;
-        uint8_t j = UINT8_MAX;
-        int32_t noise = rand_soft()%3;
-        /*uint8_t dist = get_dist_by_id(kilo_uid == 6 ? 1 : kilo_uid+1, HIST);*/
+    // process all the received messages
+    while(mydata->n_messages > 0) {
+        // get the received data
+        uint8_t *data = mydata->received_msg[mydata->n_messages-1].msg.data;
 
-        // collision avoidance
-        if(find_nearest_neighbor(&j) < min_dist && (j == kilo_uid+1 || (kilo_uid == 6 && j == 1))) {
-        /*if(dist < min_dist) {*/
-            set_motion(STOP);
-            mydata->cycle_dt = 10;
-            printf("robot=%d neighbor=%d\n", kilo_uid, j);
-        } else {
-            printf("robot=%d cycle_time=%d time_error=%d noise=%d\n", kilo_uid, mydata->cycle_dt, kilo_ticks - mydata->timestamp, noise);
+        // get sender id
+        uint16_t id = data[0] | (data[1] << 8);
 
-            // change the average speed by implementing a stop-motion fashion
-            if(kilo_ticks - mydata->timestamp < (mydata->cycle_dt+noise)*32) {
-                circulate_leader();
-            } else if(kilo_ticks - mydata->timestamp < (mydata->cycle_dt+noise+2)*32) {
-                set_motion(STOP);
-            } else {
-                mydata->timestamp = kilo_ticks;
-                mydata->cycle_dt = mydata->cycle_dt > 1 ? mydata->cycle_dt-1 : 1;
-            }
+        // search the neighbor list by id
+        for(i = 0; i < mydata->n_neighbors; i++) {
+            // found it
+            if(mydata->neighbors[i].id == id) break;
+        }
+
+        // this neighbor is not in list
+        if(i == mydata->n_neighbors) {
+            // if we have too many neighbors, we overwrite the last entry
+            // sloppy but better than overflow
+            if(mydata->n_neighbors < MAXN-1) mydata->n_neighbors++;
+        }
+
+        // i now points to where this message should be stored
+        mydata->neighbors[i].id = id;
+        mydata->neighbors[i].timestamp = kilo_ticks;
+        mydata->neighbors[i].dist = mydata->received_msg[mydata->n_messages-1].dist;
+        mydata->neighbors[i].n_neighbors = data[2];
+
+        // decrease the number of received messages
+        mydata->n_messages--;
+    }
+
+    // go through the list of neighbors, remove entries older than a threshold,
+    // currently 2 seconds.
+    for(i = mydata->n_neighbors-1; i >= 0; i--) {
+        // 32 ticks = 1 s
+        if (kilo_ticks - mydata->neighbors[i].timestamp > 2*32) {
+            // this one is too old.
+            mydata->neighbors[i] = mydata->neighbors[mydata->n_neighbors-1];
+
+            // replace it by the last entry
+            mydata->n_neighbors--;
         }
     }
 
-    // search for a light source
-    /*goto_light();*/
+    // nullify the new message indicator
+    mydata->new_message = 0;
+}
 
-    // compute the gradient value
-    compute_gradient();
-    set_color(colors[mydata->gradient%10]);
+// callback for message reception. This callback is triggered every time a message
+// is successfully decoded
+void message_rx(message_t *m, distance_measurement_t *d) {
+    mydata->new_message = 1;
 
-    // fill and send a message
-    setup_message();
+    // increase the number of received messages
+    mydata->n_messages = mydata->n_messages < MAXMSG-1 ? mydata->n_messages + 1 : 1;
 
-    // increase iteration counter (used to monitors the history of each robot)
-    inc_iter();
+    // store received message
+    mydata->received_msg[mydata->n_messages-1].msg = *m;
+    mydata->received_msg[mydata->n_messages-1].dist = estimate_distance(d);
+}
+
+// callback for message transmission. This callback is triggered every time a
+// message is scheduled for transmission (roughly twice every second)
+message_t *message_tx() {
+    if (mydata->message_lock) return 0;
+
+    return &mydata->transmit_msg;
 }
 
 #ifdef SIMULATOR
@@ -120,9 +155,8 @@ char *botinfo() {
 
     // show the neighbours and their distance to the current robot
     for(size_t j = 0; j < mydata->n_neighbors; j++) {
-
-        if(j + 1 < mydata->n_neighbors) str_len = sprintf(str_p, "%d:%d,", mydata->neighbors[j].id, get_neighbor_dist(j, HIST));
-        else str_len = sprintf(str_p, "%d:%d", mydata->neighbors[j].id, get_neighbor_dist(j, HIST));
+        if(j + 1 < mydata->n_neighbors) str_len = sprintf(str_p, "%d:%d,", mydata->neighbors[j].id, mydata->neighbors[j].dist);
+        else str_len = sprintf(str_p, "%d:%d", mydata->neighbors[j].id, mydata->neighbors[j].dist);
 
         str_p += str_len;
     }
@@ -130,17 +164,8 @@ char *botinfo() {
     // get current pose of the robot
     pose_t pose = get_pose();
 
-    str_len = sprintf(str_p, "] pose=[x:%.2f,y:%.2f,yaw:%.2f]\nlight=%d mylight=[", pose.x, pose.y, pose.yaw*180/PI, get_ambientlight());
-    str_p += str_len;
-
-    for(size_t i = 0; i < HIST; i++) {
-        if(i + 1 < HIST) str_len = sprintf(str_p, "%lu:%d,", i, get_light_level(i));
-        else str_len = sprintf(str_p, "%lu:%d]", i, get_light_level(i));
-
-        str_p += str_len;
-    }
-
-    str_len = sprintf(str_p, " type=%d gradient=%d", mydata->bot_type, mydata->gradient);
+    // show the robot coordinates on Cartesian space
+    str_len = sprintf(str_p, "] pose=[x:%.2f,y:%.2f,yaw:%.2f]\nlight=%d", pose.x, pose.y, pose.yaw*180/PI, get_ambientlight());
     str_p += str_len;
 
     return botinfo_buffer;
@@ -153,13 +178,36 @@ int16_t callback_lighting(double x, double y) {
     double light_x = 1;
     double light_y = 1;
     double light_radius = 1000;
-    double light_dist = norm(x-light_x, y-light_y);
+    double light_dist = sqrt(pow(x-light_x, 2) + pow(y-light_y, 2));
 
     if(light_dist <= light_radius) return (1 - light_dist/light_radius)*1023;
 
     return 0;
 }
 #endif
+
+/******************************************************************************
+ * Editable Part of the Code
+ * Put your personal code in the functions bellow.
+ *****************************************************************************/
+
+// put your setup code here, to be run only once
+void setup() {
+    // set the number of neighbors to zero
+    mydata->n_neighbors = 0;
+
+    // set the number of messages to zero
+    mydata->n_messages = 0;
+}
+
+// put your main code here, will be run repeatedly
+void loop() {
+    // build message
+    setup_message();
+
+    // process received message
+    process_message();
+}
 
 // main function
 int main(void) {
@@ -183,11 +231,8 @@ int main(void) {
     // start kilobot event loop
     kilo_start(setup, loop);
 
-    // initialize ring buffer
-    RB_init();
-
     // register message callback
-    kilo_message_rx = rxbuffer_push;
+    kilo_message_rx = message_rx;
 
     // register message transmission callback
     kilo_message_tx = message_tx;
